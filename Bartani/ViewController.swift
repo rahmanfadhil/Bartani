6301db67562d82a6cc26fef513c911d22d8fd7be
Rahman Fadhil <rhmnfadhil@gmail.com>
2020-07-17 11:03:24 +0700

    Initial commit

---
+++ Bartani/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bartani
+//
+//  Created by Rahman Fadhil on 17/07/20.
+//  Copyright © 2020 Rahman Fadhil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
