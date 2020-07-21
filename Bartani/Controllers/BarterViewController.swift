f9eb73e5bdfc6fa11aa4abf1dad97b03b50513f6
Rahman Fadhil <rhmnfadhil@gmail.com>
2020-07-21 10:55:09 +0700

    Add product list in barter page

--- Bartani/Controllers/BarterViewController.swift
+++ Bartani/Controllers/BarterViewController.swift
@@ -8,7 +8,18 @@
 
 import UIKit
 
-class BarterViewController: UIViewController {
+class BarterViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    var products = [
+        Product(title: "Bayam", price: 5500, quantity: "1 ikat", image: UIImage(named: "bayam")),
+        Product(title: "Cabai", price: 7000, quantity: "1,5 kg", image: UIImage(named: "cabai")),
+        Product(title: "Kentang", price: 8000, quantity: "5 buah", image: UIImage(named: "kentang")),
+        Product(title: "Kangkung", price: 3000, quantity: "2 ikat", image: UIImage(named: "kangkung")),
+        Product(title: "Bayam", price: 5500, quantity: "1 ikat", image: UIImage(named: "bayam")),
+        Product(title: "Cabai", price: 7000, quantity: "1,5 kg", image: UIImage(named: "cabai")),
+        Product(title: "Kentang", price: 8000, quantity: "5 buah", image: UIImage(named: "kentang")),
+        Product(title: "Kangkung", price: 3000, quantity: "2 ikat", image: UIImage(named: "kangkung"))
+    ]
     
     var product: Product?
 
@@ -17,6 +28,7 @@ class BarterViewController: UIViewController {
     @IBOutlet weak var productPriceLabel: UILabel!
     @IBOutlet weak var productTitleLabel: UILabel!
     @IBOutlet weak var productQuantityLabel: UILabel!
+    @IBOutlet weak var productCollectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,6 +43,12 @@ class BarterViewController: UIViewController {
         if let price = product?.price {
             productPriceLabel.text = "Rp \(price)"
         }
+        
+        productCollectionView.delegate = self
+        productCollectionView.dataSource = self
+        
+        let nib = UINib(nibName: "ProductCollectionViewCell", bundle: nil)
+        productCollectionView.register(nib, forCellWithReuseIdentifier: "productCell")
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -39,6 +57,21 @@ class BarterViewController: UIViewController {
     
     override func viewWillDisappear(_ animated: Bool) {
         navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "productCell", for: indexPath) as! ProductCollectionViewCell
+        let product = products[indexPath.row]
+        cell.titleLabel.text = product.title
+        cell.priceLabel.text = "Rp \(product.price)"
+        cell.thumbnailImage.image = product.image
+        cell.amountLabel.text = product.quantity
+        cell.boxView.backgroundColor = UIColor.systemGray6
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return products.count
     }
     
 
