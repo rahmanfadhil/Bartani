9c12f82808552d237f300d2fe23f84876097343a
rahman <rahmanarif.9764@gmail.com>
2020-07-27 09:55:02 +0700

    create storyboard RequestDetail with ViewController

---
+++ Bartani/Controllers/DetailRequestViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailRequestViewController.swift
+//  Bartani
+//
+//  Created by Arif Rahman Sidik on 27/07/20.
+//  Copyright © 2020 Rahman Fadhil. All rights reserved.
+//
+
+import UIKit
+
+class DetailRequestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
